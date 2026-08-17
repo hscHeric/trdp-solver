e412c374a0cd94c1f22390953cf13beb3969ba4a
Heric da Silva Cruz <hericsilvaho@gmail.com>
2026-08-16 21:36:23 -0300

    adicionando assinatura do decoder

---
+++ metaheuristics/trdp/trdp_decoder.hpp
@@ -0,0 +1,21 @@
+#ifndef TDRP_DECODER_H
+#define TDRP_DECODER_H
+
+#include "../common/graph.hpp"
+#include <atomic>
+#include <memory>
+#include <vector>
+
+class TDRPDecoder
+{
+public:
+	explicit TDRPDecoder(const graph *graph);
+
+	double decode(const std::vector<double> &chromosome) const;
+
+private:
+	const graph *graph_;
+	std::shared_ptr<std::atomic<uint64_t>> evaluation_counter;
+};
+
+#endif
